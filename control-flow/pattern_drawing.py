74d00e774ea0ac27c86db1f41fb50d6d594e5345
Ephraim Mwangi <mwangiephraim173@gmail.com>
2025-09-26 12:43:17 +0300

    print formulae update

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,6 +2,4 @@ row = int(input("Enter the size of the pattern:"))
 while row <= 0:
     row= int (input("Enter a positive integer:"))
 for _ in range(row):
- print("*" * row)
-
-    
+ print("*", end="")
